1d0feb71debff9daf31110991ff69de8e17c0501
Manos Pappas <mano.pappa@gmail.com>
2021-06-26 17:36:23 +0100

    Serving React App in Production: - Wrap both server and client with a package.json with script to run both and build react app in server's public directory - Run both with one command and server index.html from the builded react on run

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 
@@ -11,6 +12,11 @@ app.use(
   })
 );
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
+
 app.use(planetsRouter);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
