b329d14ad0a7ea07931ccff59dda93cf4d5cac12
Manos Pappas <mano.pappa@gmail.com>
2021-09-12 19:35:41 +0100

    Add complete API functionality and integration with Front-End

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,8 @@
-const http = require("http");
+const http = require('http');
 
-const app = require("./app");
+const app = require('./app');
 
-const { loadPlanetsData } = require("./models/planets.model");
+const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
@@ -12,7 +12,7 @@ async function startServer() {
   await loadPlanetsData();
 
   server.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+    console.log(`Listening on port ${PORT}...`);
   });
 }
 
