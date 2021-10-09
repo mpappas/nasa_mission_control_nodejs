d22059892182b02dec64104e6931609520348f59
Manos Pappas <mano.pappa@gmail.com>
2021-10-09 17:29:14 +0100

    Initial graphql example

---
+++ graphql-example/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const { buildSchema } = require('graphql');
+const { graphqlHTTP } = require('express-graphql');
+
+const schema = buildSchema(`
+    type Query {
+        description: String
+        price: Float
+    }
+`);
+
+const root = {
+  description: 'Red Shoe',
+  price: 42.12,
+};
+
+const app = express();
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+
+app.listen(3000, () => {
+  console.log('Running GraphQL server ...');
+});
